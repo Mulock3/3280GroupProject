9e5d20bc43a647f08cfc86f52a24d28a2229aaa5
Austin Langston <41969793+Mulock3@users.noreply.github.com>
2021-03-17 08:32:41 -0600

    Changing to WPF
    Still uses .NET Framework

--- GroupProject/GroupProject3280/Properties/Resources.Designer.cs
+++ GroupProject/GroupProject3280/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace GroupProject.Properties
+namespace GroupProject3280.Properties
 {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -43,7 +43,7 @@ namespace GroupProject.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GroupProject.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GroupProject3280.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
